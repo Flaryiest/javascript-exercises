5eea3e6933493e102197d447c9f2ae50e4b9bf78
Flaryiest <ericmzuo1@gmail.com>
2024-02-12 16:35:45 -0700

    finished js exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(termOfSequence) {
+    if (termOfSequence < 0) {
+        return "OOPS"
+    }
+    sequence = [0,1];
+    finalResult = 0
+    for (currentTerm = 0; currentTerm <= termOfSequence - 2; currentTerm++) {
+        sequence.push((sequence[currentTerm] + sequence[currentTerm + 1]))
+        console.log(sequence)
+    }
+    return sequence[termOfSequence]
 
 };
 
