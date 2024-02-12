9c501b0e5e56cc0deb6051b6dc8694a985e1da57
Flaryiest <ericmzuo1@gmail.com>
2024-02-11 20:28:47 -0700

    finished exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function(word) {
+    charactorsToCheck = word.length;
+    reversedWord = word.split("").reverse().join().toLowerCase();
+    reversedWord = reversedWord.replaceAll(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+    reversedWord = reversedWord.replaceAll(" ", "")
+    word = word.replaceAll(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    word = word.replaceAll(" ", "")
+    word = word.toLowerCase()
+    console.log(word, reversedWord)
+    for (let character = 0; character < charactorsToCheck; character++) {
+        if (word[character] != reversedWord[character]) {
+            console.log(word[character], reversedWord[character])
+            return false
+        }
+    }
+    return true
 
 };
 
